1eaaedc79be67842d1dbde4be62f8892e4f048ff
Jackson Price <price.jacksonk@gmail.com>
2023-03-09 20:01:03 -0600

    Add: Leave class to handle leave voice state update events

---
+++ lib/discord/listener/voice_state_update/leave.rb
@@ -0,0 +1,32 @@
+module Discord
+  module Listener
+    module VoiceStateUpdate
+      # handle voice state update leave event
+      class Leave
+
+        def initialize(event)
+          @event = event
+        end
+
+        def perform
+          TwilioClient.send_messages(message)
+        end
+
+        private
+
+        def users
+          @event.old_channel.users.map(&:username)
+        end
+
+        def message
+          emoji = "\u{1F44B}"
+          connected_emoji = "\u{1F7E2}"
+          channel = @event.old_channel.name
+          user_count = "There is no one else in this channel.\n"
+
+          text_message = "#{@event.user.username} #{emoji} left the voice channel #{channel}.\n\n#{user_count}"
+        end
+      end
+    end
+  end
+end
