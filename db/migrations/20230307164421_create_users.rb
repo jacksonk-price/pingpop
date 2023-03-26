6982d547c4fd45bc4d4d4fefc44137f9de346967
Jackson Price <price.jacksonk@gmail.com>
2023-03-26 17:25:29 -0500

    Fix: migration issue

--- db/migrations/20230307164421_create_users.rb
+++ db/migrations/20230307164421_create_users.rb
@@ -1,7 +1,7 @@
 require '../db_setup'
-class CreateMembers < ActiveRecord::Migration[7.0]
+class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :members do |t|
+    create_table :users do |t|
       t.integer :discord_id
       t.string :display_name
       t.string :phone_number
